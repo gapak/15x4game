30b3f74719151f40d4a4c24d8f070e814fa59c5c
IvanMagda <iv.y.magda@gmail.com>
2016-08-08 17:35:18 +0300

    Fixed side panel
    collapse click refers to panel

--- app/LogPanel.js
+++ app/LogPanel.js
@@ -2,7 +2,7 @@ LogPanel = {
 	day: 0,
 	message: [],
 	clear: function(){this.message = []},
-	hide: $(this).on('click', function () {
+	hide: function(){
 		$('#main_content').toggleClass('main_content80 main_content100');
-	})
+	}
 }
