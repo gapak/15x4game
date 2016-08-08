6973a0bce183460689923612153c89777f0471b5
VedminaTanya <Tanya_Norilsk@mail.ru>
2016-08-08 15:08:56 +0300

    add context to the TOP60

--- app/Lecture.js
+++ app/Lecture.js
@@ -1,6 +1,8 @@
 
-function Lecture(name, text) {
+function Lecture(lector_name, name, text, url) {
+    this.lector_name = lector_name;
     this.name = name;
     this.text = text;
+    this.url = url;
     this.is_performed = 0;
 }
