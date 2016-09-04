0c8184dec69531a951727efd5f9d2fa204432f82
Daria Grishchenko <amseledka@gmail.com>
2016-09-04 12:36:41 +0300

    Log settings styles

--- app/LogPanel.js
+++ app/LogPanel.js
@@ -1,44 +1,44 @@
 LogPanel = {
-	day: 0,
-	messages: [],
-	clear: function(){this.messages = []},
-	hide: function(){
-		$('#main_content').toggleClass('main_content80 main_content100');
-	}
+    day: 0,
+    messages: [],
+    clear: function(){this.messages = []},
+    hide: function(){
+        $('#main_content').toggleClass('main_content80 main_content100');
+    }
 };
 
 function LogMessage(filter, text) {
-	this.filter = filter;
-	this.text = text;
+    this.filter = filter;
+    this.text = text;
 }
 
 LogPanel.filters = ["Badge", "Not enough", "Paid", "Gained", "Reward", "Max", "lecturer", "Lecturer"];
 
 var check_html ="";
 LogPanel.filters.forEach(function (filter) {
-	check_html += '<input type="checkbox" name="log_filter" checked="checked" value="'+filter+'">'+filter;
+    check_html += '<div class="checkbox"><label><input type="checkbox" name="log_filter" checked="checked" value="'+filter+'">'+filter+'</label></div>';
 });
 
 document.addEventListener("DOMContentLoaded", function() {
-	document.getElementById("log_panel_filters").insertAdjacentHTML('beforeend', check_html);
+    document.getElementById("log_panel_filters").insertAdjacentHTML('beforeend', check_html);
 });
 
 function FilterLogs(){
-	var check = document.getElementsByName("log_filter");
+    var check = document.getElementsByName("log_filter");
     for (var key in check) {
         var item = check[key];
-		if(item.checked==true){
-			LogPanel.messages.forEach(function(logMessage){
-				if(logMessage.text.includes(item.value)){
-					logMessage.filter = true;
-				}
-			});
-		}else{
-			LogPanel.messages.forEach(function(logMessage){
-				if(logMessage.text.includes(item.value)){
-					logMessage.filter = false;
-				}
-			});
-		}
-	}
+        if(item.checked==true){
+            LogPanel.messages.forEach(function(logMessage){
+                if(logMessage.text.includes(item.value)){
+                    logMessage.filter = true;
+                }
+            });
+        }else{
+            LogPanel.messages.forEach(function(logMessage){
+                if(logMessage.text.includes(item.value)){
+                    logMessage.filter = false;
+                }
+            });
+        }
+    }
 }
